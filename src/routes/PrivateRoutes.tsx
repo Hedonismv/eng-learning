55a9e68a9c0694c463abf0a1c59bd7bcd549c80d
Vitaly Klyhun <klygun90@gmail.com>
2022-04-25 00:41:30 +0300

    new pages added

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {Navigate} from "react-router"
-import Main from "../Pages/Main/Main";
+import Main from "../Pages/Public/Main";
+import Lessons from "../Pages/Private/Lessons";
+import About from "../Pages/Public/About";
 
 const PrivateRoutes = () => {
-
 
     return (
         <Routes>
             <Route path={'/'} element={<Main/>}/>
+            <Route path={'/about'} element={<About/>}/>
+            <Route path={'/lessons'} element={<Lessons/>}/>
             <Route path={'*'} element={<Navigate to={'/'}/>}/>
         </Routes>
     );
