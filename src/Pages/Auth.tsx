71a34082268deaf109f03983ba55aed8630039af
Vitaly Klyhun <klygun90@gmail.com>
2022-04-24 19:33:15 +0300

    Error handling, email verification modify

--- src/Pages/Auth.tsx
+++ src/Pages/Auth.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import {
     Box,
     Button,
@@ -20,9 +20,15 @@ import {
 } from "react-firebase-hooks/auth";
 import {auth} from "../firebaseSettings/firebaseConsts";
 import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 interface AuthProps {
     login: boolean
+}
+
+type Errors = {
+    message: string | undefined,
+    isError: boolean
 }
 
 const Auth:FC<AuthProps> = ({login}) => {
@@ -30,20 +36,21 @@ const Auth:FC<AuthProps> = ({login}) => {
     //Google auth
     const [signInWithGoogle] = useSignInWithGoogle(auth);
 
-    //Redux actions
+    //Redux actions & State
     const {setLoggedUser} = useActions()
-
+    const {loggedUser} = useTypedSelector(state => state.user)
 
     //UI consts
-    const [verify, setVerify] = useState(false)
+    const [verify, setVerify] = useState<Boolean>(false)
     const [uiError, setUiError] = useState<String>('')
+    const [authError, setAuthError] = useState<Errors>({isError: false, message: ''})
 
     //Register hook
     const [
         createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
+        userR,
+        loadingR,
+        errorR,
     ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
 
     //login hook
@@ -55,38 +62,67 @@ const Auth:FC<AuthProps> = ({login}) => {
     ] = useSignInWithEmailAndPassword(auth);
 
     //React-hook-form
-    const {register, handleSubmit, formState: { errors } } = useForm<FieldValues>();
+    const {register, handleSubmit, reset, formState: { errors } } = useForm<FieldValues>();
     const {email, password, password2} = errors;
 
     //Login with email and password function
     const loginWP = (data:FieldValues) => {
         signInWithEmailAndPassword(data.email, data.password)
-          .catch(error => console.log(error.message))
+          .catch(error => {
+              if(error.code === 400){
+                  setAuthError({isError: true, message: 'Email or password incorrect'})
+              }
+          })
     }
+
+    //Error Register & Login handler
+    useEffect(() => {
+        if(errorR || errorL){
+            const chanError = errorR ? errorR : errorL
+            setAuthError({isError: true, message: chanError?.message})
+            setVerify(false)
+        }else{
+            setAuthError({isError: false, message: ''})
+        }
+    }, [errorR, errorL, loadingR])
+
 
 
     //Register with email and password function
     const registerWP = (data:FieldValues) => {
         if(data.password === data.password2){
             createUserWithEmailAndPassword(data.email, data.password)
-              .then(() => setVerify(true))
-              .catch(error => console.log(error.message))
+            setVerify(true)
         }else{
-            return
+            setAuthError({isError: true, message: 'Passwords dont match'})
         }
     }
 
 
     //Function which check email verification
     const verifiedEmail = () => {
-        user?.user.reload()
-        console.log(user?.user.emailVerified);
-        if(user?.user.emailVerified){
-            setLoggedUser(user.user)
-        }else{
-            setUiError('Hmm... Email no verified, are you confident that you verify it?')
-        }
+        console.log(loggedUser);
+        loggedUser?.reload()
+          .then(() => {
+              setLoggedUser(loggedUser)
+          })
+          .catch(error => error.message)
+          .finally(() => {
+              if(!loggedUser?.emailVerified){
+                  setUiError('Hmm... Email no verified, are you confident that you verify it?')
+              }
+          })
     }
+
+
+    //Effect hook to handle email-verifying
+    useEffect(() => {
+        setAuthError({isError: false, message: ''})
+        reset({email, password, password2})
+        if(loggedUser && !loggedUser.emailVerified){
+            setVerify(true)
+        }
+    },[loggedUser])
 
 
     if(verify){
@@ -106,18 +142,19 @@ const Auth:FC<AuthProps> = ({login}) => {
     return (
         <Container maxW={"container.lg"}>
             <Box alignItems={"center"} display={"flex"} height={"80vh"} justifyContent={"center"}>
-                <FormControl w={[300,400,500]} as={"form"} onSubmit={login ? handleSubmit(loginWP) : handleSubmit(registerWP)} isInvalid={email || password || password2}>
+                <FormControl w={[300,400,500]} as={"form"} onSubmit={login ? handleSubmit(loginWP) : handleSubmit(registerWP)} isInvalid={email || password || password2 || authError.isError}>
                     <FormLabel htmlFor='first-name'>{login ? 'Login' : 'Register'}</FormLabel>
                     <VStack spacing={5}>
                         <Input id='email' type={"email"} placeholder='Your email' {...register('email', {required: true})}/>
-                        {errors.email && <FormErrorMessage>This field is required</FormErrorMessage>}
+                        {errors.email && <FormErrorMessage>To register you, we need your email</FormErrorMessage>}
                         <Input id='password' placeholder='Password' {...register('password', {required: true})} type={"password"} />
-                        {errors.password && <FormErrorMessage>This field is required</FormErrorMessage>}
+                        {errors.password && <FormErrorMessage>Hey, write your password here</FormErrorMessage>}
                         {!login ? <Input id='password2' placeholder='Confirm your password' type={"password"} {...register('password2', {required: true})} /> : null}
-                        {errors.password2 && <FormErrorMessage>This field is required</FormErrorMessage>}
+                        {errors.password2 && <FormErrorMessage>Hey, write your password here</FormErrorMessage>}
+                        {authError.isError && <FormErrorMessage>{authError.message}</FormErrorMessage>}
                         <Button
                             type={"submit"}
-                            isLoading={loadingL}
+                            isLoading={login ? loadingL : loadingR}
                             width={"50%"} cursor={"pointer"}
                             _hover={{backgroundColor: "teal.500", color: "white"}}
                         >{login ? 'Login' : 'Register'}</Button>
