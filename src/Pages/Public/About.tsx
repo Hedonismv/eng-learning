9d8b77e0d678c3d128beb9b7fbf6b2d43e6c773c
Vitaly Klyhun <klygun90@gmail.com>
2022-04-25 00:39:18 +0300

    Initial: create FC:

---
+++ src/Pages/Public/About.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Text } from "@chakra-ui/react";
+
+const About:FC = () => {
+	return (
+		<div>
+			<Text>About team</Text>
+		</div>
+	);
+};
+
+export default About;
