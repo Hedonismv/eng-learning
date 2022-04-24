db0b3c882132b73554392d9d7a3dd455276f29ec
Vitaly Klyhun <klygun90@gmail.com>
2022-04-25 00:41:50 +0300

    new pages added

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom"
-import Main from "../Pages/Main/Main";
+import Main from "../Pages/Public/Main";
 import {Navigate} from "react-router";
-import Auth from "../Pages/Auth";
+import Auth from "../Pages/Public/Auth";
+import About from "../Pages/Public/About";
 
 
 
@@ -10,6 +11,7 @@ const PublicRoutes = () => {
     return (
         <Routes>
             <Route path={'/'} element={<Main/>}/>
+            <Route path={'/about'} element={<About/>}/>
             <Route path={'/login'} element={<Auth login={true}/>}/>
             <Route path={'/register'} element={<Auth login={false}/>}/>
             <Route path={'*'} element={<Navigate to={'/'}/>}/>
