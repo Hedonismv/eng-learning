a09a8a6ddd322ad4771d691c1a7e0a835106a80f
Vitaly Klyhun <klygun90@gmail.com>
2022-04-27 18:31:18 +0300

    item for answering

---
+++ src/Components/Lessons/AnswerItem.tsx
@@ -0,0 +1,36 @@
+import React, { Dispatch, FC, SetStateAction } from "react";
+import {Box,Text} from "@chakra-ui/react";
+
+export interface AnswerItemProps {
+	engWord: {
+		word: string,
+		answerId: number
+	},
+	validateAnswer: (id:number) => void
+}
+
+const AnswerItem:FC<AnswerItemProps> = ({engWord, validateAnswer}) => {
+
+
+	return (
+		<Box
+			onClick={() => validateAnswer(engWord.answerId)}
+			backgroundColor={"teal.100"}
+			borderRadius={10}
+			width={['35%', '30%', '40%']}
+			maxH={"2xs"}
+			py={3}
+			px={10}
+			display={"flex"}
+			alignItems={"center"}
+			justifyContent={"center"}
+			my={5}
+			mx={5}
+			cursor={"pointer"}
+		>
+			<Text>{engWord.word}</Text>
+		</Box>
+	);
+};
+
+export default AnswerItem;
