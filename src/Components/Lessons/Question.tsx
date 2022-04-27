7bff38ec44e40c121f2e01d491915b8da76ad838
Vitaly Klyhun <klygun90@gmail.com>
2022-04-27 18:35:23 +0300

    question rendering

--- src/Components/Lessons/Question.tsx
+++ src/Components/Lessons/Question.tsx
@@ -1,6 +1,8 @@
-import React, { FC, useState } from "react";
-import { Container, Text, Box } from "@chakra-ui/react";
+import React, { FC, useEffect, useState } from "react";
+import { Container, Text, Box, Button, VStack } from "@chakra-ui/react";
 import {DocumentData} from 'firebase/firestore/'
+import AnswerItem from "./AnswerItem";
+import { QuestionData } from "../../types/project";
 
 interface QuestionProps {
 	queData: DocumentData[] | undefined
@@ -11,13 +13,39 @@ const Question:FC<QuestionProps> = ({queData}) => {
 
 	const [queNum, setQueNum] = useState<number>(0)
 
-	console.log(queData);
+	const {transWord, engWords, questionAnsId} = queData![queNum]
+
+	//validateHandler
+	const validateHandler = (id:number) => {
+		if(id === questionAnsId){
+			handleSkip()
+			console.log('RIGHT');
+		}else{
+			console.log('Incorrect');
+		}
+	}
+
+	//Skip handler
+	const handleSkip = () => {
+		const queLength = queData?.length! - 1
+		if(queNum === queLength ){
+			setQueNum(0)
+		}else{
+			setQueNum(prevState => prevState + 1)
+		}
+	}
 
 	return (
 		<Container maxW={"container.sm"}>
-			<Box>
-				<Text>{queData && queData[queNum].transWord}</Text>
-			</Box>
+			<VStack>
+			<Text fontSize={"xx-large"}>{transWord}</Text>
+				<Box display={"flex"} justifyContent={"space-between"} flexWrap={"wrap"}>
+					{engWords.map((ew:any) =>
+						<AnswerItem engWord={ew} key={ew.answerId} validateAnswer={validateHandler}/>
+					)}
+				</Box>
+				<Button onClick={() => handleSkip()} width={[120, 200, 250]} backgroundColor={"red.500"}>Пропустить</Button>
+			</VStack>
 		</Container>
 	);
 };
