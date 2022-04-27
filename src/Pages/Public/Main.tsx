89ca5f979b8a45d7531ad3a68f1ce2249bdca6f8
Vitaly Klyhun <klygun90@gmail.com>
2022-04-27 18:34:59 +0300

    test link

--- src/Pages/Public/Main.tsx
+++ src/Pages/Public/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Container, Text} from "@chakra-ui/react";
+import StyledLink from "../../Components/StyledLink";
 
 const Main = () => {
     return (
         <Container maxW={"container.lg"}>
             <Text>Main Page</Text>
+            <StyledLink to={'/lessons'}>Start Learning</StyledLink>
         </Container>
     );
 };
