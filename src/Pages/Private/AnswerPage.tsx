ec94e2b65e45a2a3747caedcca0c03a5ffb8d6a9
Vitaly Klyhun <klygun90@gmail.com>
2022-05-03 16:30:18 +0300

    add CorrectIncorrect comp and refactor component

--- src/Pages/Private/AnswerPage.tsx
+++ src/Pages/Private/AnswerPage.tsx
@@ -6,30 +6,26 @@ import { collection } from "firebase/firestore";
 import { firestore } from "../../firebaseSettings/firebaseConsts";
 import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
 import Question from "../../Components/Lessons/Question";
-import { useActions } from "../../hooks/useActions";
+import CorrectIncorrect from "../../Components/CorrectIncorrect";
 
 const AnswerPage:FC = () => {
 
 	const {lvl, mode} = useParams()
-	const {addQuestion} = useActions()
 
 	const questionRef = collection(firestore,`lessons/english/${lvl}/${mode}/questions`)
 	const [values, loading, error] = useCollectionDataOnce(questionRef)
 
-
-	useEffect(() => {
-		values && addQuestion(values)
-	},[values])
-
-
 	return (
-		<Container height={"80vh"} maxW={"container.lg"} display={"flex"} flexDirection={"column"}>
-			<Text as={"h1"} fontSize={["xl", "xx-large"]} fontWeight={"bold"} textAlign={"center"} my={5}>Выбери правильный вариант из приведенных слов</Text>
-			<Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} mt={[10,20]}>
-				{loading ? <Spinner/> : <Question queData={values}/>}
-				{error && <Text>{error.message}</Text>}
-			</Box>
-		</Container>
+		<Box height={'90vh'}>
+			<Container maxW={"container.lg"} display={"flex"} flexDirection={"column"}>
+				<Text as={"h1"} fontSize={["xl", "xx-large"]} fontWeight={"bold"} textAlign={"center"} my={5}>Выбери правильный вариант из приведенных слов</Text>
+				<Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} mt={[2,5]}>
+					{loading ? <Spinner/> : <Question queData={values}/>}
+					{error && <Text>{error.message}</Text>}
+				</Box>
+			</Container>
+			<CorrectIncorrect/>
+		</Box>
 	);
 };
 
