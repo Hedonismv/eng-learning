56ae1361e0bebf62a8cc8b6d1e2d433e16c91538
Vitaly Klyhun <klygun90@gmail.com>
2022-05-04 10:47:12 +0300

    added favicon

--- src/Pages/Public/Main.tsx
+++ src/Pages/Public/Main.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Container, Text} from "@chakra-ui/react";
+import {Container, Text, Box, HStack} from "@chakra-ui/react";
 import StyledLink from "../../Components/StyledLink";
 
 const Main = () => {
     return (
-        <Container maxW={"container.lg"}>
-            <Text>Main Page</Text>
-            <StyledLink to={'/lessons'}>Start Learning</StyledLink>
-        </Container>
+        // <Container maxW={"container.lg"}>
+        //     <Text>Main Page</Text>
+        //     <StyledLink to={'/lessons'}>Start Learning</StyledLink>
+        // </Container>
+        <HStack height={'100vh'} width={'100%'}>
+            <Box width={'40%'}>
+                <h1>Image</h1>
+            </Box>
+            <Box width={'60%'} height={'100%'} backgroundColor={'black'}>
+                <h1>Info</h1>
+            </Box>
+        </HStack>
     );
 };
 
