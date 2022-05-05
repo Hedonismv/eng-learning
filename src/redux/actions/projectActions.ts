5689a217af6d1f47122ebcb75e2958784af521bc
Vitaly Klyhun <klygun90@gmail.com>
2022-05-05 18:42:22 +0300

    statistic interface, new type Const and func interfaces (addQuestion, completeProgram, reloadProgram)

--- src/redux/actions/projectActions.ts
+++ src/redux/actions/projectActions.ts
@@ -1,3 +1,9 @@
 import { ProjectAction, ProjectActionTypes } from "../../types/project";
 
-export const setAnswer = (answer: boolean):ProjectAction => ({type: ProjectActionTypes.SET_ANSWER, payload: answer});
+export const setAnswer = (answer: boolean | null):ProjectAction => ({type: ProjectActionTypes.SET_ANSWER, payload: answer});
+
+export const addQuestion = (qty: number):ProjectAction => ({type: ProjectActionTypes.ADD_QUESTION, payload: qty});
+
+export const completeProgram = (isCompleted: boolean):ProjectAction => ({type: ProjectActionTypes.COMPLETE_PROGRAM, payload: isCompleted});
+
+export const reloadProgram = ():ProjectAction => ({type: ProjectActionTypes.RELOAD_PROGRAM});
