11c0fd9b65ccfd822e74916d3c2ea881cdaa8b38
Vitaly Klyhun <klygun90@gmail.com>
2022-05-05 18:42:26 +0300

    statistic interface, new type Const and func interfaces (addQuestion, completeProgram, reloadProgram)

--- src/redux/reducers/projectReducer.ts
+++ src/redux/reducers/projectReducer.ts
@@ -1,18 +1,53 @@
-
-import {ProjectAction, ProjectActionTypes, ProjectState} from "../../types/project";
+import { ProjectAction, ProjectActionTypes, ProjectState } from "../../types/project";
 
 const initialState: ProjectState = {
     questionData: null,
-    isCorrectAnswer: false
+    isCorrectAnswer: null,
+    statistic:{
+        totalQuestions: 0,
+        correctQty: 0,
+        incorrectQty: 0
+    },
+    isProgramCompleted: false
 }
 
 
 export const projectReducer = (state = initialState, action: ProjectAction):ProjectState => {
     switch (action.type) {
         case ProjectActionTypes.SET_ANSWER:
+            if(action.payload){
+                return {
+                    ...state,
+                    isCorrectAnswer: action.payload,
+                    statistic: {...state.statistic, correctQty: state.statistic.correctQty! + 1}
+                }
+            }else{
+                return {
+                    ...state,
+                    isCorrectAnswer: action.payload,
+                    statistic: {...state.statistic, incorrectQty: state.statistic.incorrectQty! + 1}
+                }
+            }
+        case ProjectActionTypes.ADD_QUESTION:
             return {
                 ...state,
-                isCorrectAnswer: action.payload
+                statistic: {...state.statistic, totalQuestions: action.payload}
+            }
+        case ProjectActionTypes.COMPLETE_PROGRAM:
+            return {
+                ...state,
+                isProgramCompleted: action.payload
+            }
+        case ProjectActionTypes.RELOAD_PROGRAM:
+            return {
+                ...state,
+                isProgramCompleted: false,
+                isCorrectAnswer: null,
+                statistic: {
+                    totalQuestions: 0,
+                    correctQty: 0,
+                    incorrectQty: 0
+                }
             }
         default:
             return state
