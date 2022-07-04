94fca062b1af7c1114ce4fb2a6b737f296865cb1
Vitaly Klyhun <klygun90@gmail.com>
2022-07-04 19:17:13 +0300

    init

--- client/src/Components/Lessons/Question.tsx
+++ client/src/Components/Lessons/Question.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState } from "react";
 import { Container, Text, Box, VStack, Progress } from "@chakra-ui/react";
-import {DocumentData} from 'firebase/firestore/'
+import {DocumentData} from 'client/src/Components/Lessons/Question'
 import AnswerItem from "./AnswerItem";
 import { useActions } from "../../hooks/useActions";
 import {useSound} from 'use-sound';
---
+++ client/src/firebaseSettings/configFb.ts
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9XERyRQ0CJ84wZkAxVkdJ-GpExNR6t2M",
+    authDomain: "english-learn-df003.firebaseapp.com",
+    projectId: "english-learn-df003",
+    storageBucket: "english-learn-df003.appspot.com",
+    messagingSenderId: "542344103967",
+    appId: "1:542344103967:web:cc45c493f4cdd5e0f92d2a"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
